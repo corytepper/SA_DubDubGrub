f2e26d09c5abd460984c202a518f7e516a0f4adb
Cory Tepper <corytepper@gmail.com>
2022-11-28 20:14:13 -0500

    Initial Commit

---
+++ SA_DubDubGrub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SA_DubDubGrub
+//
+//  Created by Cory Tepper on 11/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SA_DubDubGrub/SA_DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SA_DubDubGrubApp.swift
+//  SA_DubDubGrub
+//
+//  Created by Cory Tepper on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SA_DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
