b7ff6fd60ded42a55efb865ccccd3eb03c094f7d
Cory Tepper <corytepper@gmail.com>
2023-01-07 09:19:23 -0500

    Layout UI on LocationDetailView.

--- SA_DubDubGrub/LocationListView.swift
+++ SA_DubDubGrub/LocationListView.swift
@@ -26,16 +26,17 @@ struct LocationListView: View {
                                 .fontWeight(.semibold)
                                 .lineLimit(1).minimumScaleFactor(0.75)
                             HStack {
-                                AvatarView()
-                                AvatarView()
-                                AvatarView()
-                                AvatarView()
-                                AvatarView()
+                                AvatarView(size: 35)
+                                AvatarView(size: 35)
+                                AvatarView(size: 35)
+                                AvatarView(size: 35)
+                                AvatarView(size: 35)
                             }
                             
                         }
                         .padding(.leading)
                     }
+                    
                 }
             }
             
@@ -51,11 +52,14 @@ struct LocationListView_Previews: PreviewProvider {
 }
 
 struct AvatarView: View {
+    
+    var size: CGFloat
+    
     var body: some View {
         Image("default-avatar")
             .resizable()
             .scaledToFit()
-            .frame(width: 35, height: 35)
+            .frame(width: size, height: size)
             .clipShape(Circle())
     }
 }
